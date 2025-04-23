16717e6681b0ee491f7e2d7ebe49b7ab64538fdf
filipalimaa <filipalima1995@gmail.com>
2025-04-23 10:15:42 +0100

    pytest and TDD

--- exercises/wordSearch/test_cobraSolution.py
+++ exercises/wordSearch/test_cobraSolution.py
@@ -1,5 +1,10 @@
 from cobraSolution import exist
 
+## Definição de vários testes: 1º em caso de existência, para perceber se o código encontra de facto palvras válidas no board; 2º em caso de inexistência da word, ou seja, para o caso em que as letras existam no board, mas não formam a palavra
+## 3º para um caso minimalista, para garantir que ele aceita qualquer tamanho da palavra; 4º testar quando realmente as letras da palavra não existem no board, para verificar se ignora letras inexistentes
+
+## Outros possíveis testes: boards maiores ou vazios (?)
+
 def test_exist_true():
     
     board = [["A","B","C","E"],
