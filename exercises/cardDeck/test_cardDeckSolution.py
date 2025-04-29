8c952dda1e70f40edfab6550421bdc5616c27bdb
filipalimaa <filipalima1995@gmail.com>
2025-04-29 11:35:28 +0100

    test: create first false test

--- exercises/cardDeck/test_cardDeckSolution.py
+++ exercises/cardDeck/test_cardDeckSolution.py
@@ -79,4 +79,13 @@ def test_example1():
     gaveta = ["6","7", "3","4", "4", "4", "4","5","5","3", "4", "7", "4","6", "6", "2","3","3","5", "4", "4","2","7","7"]
     
     assert countingCards(baralho, gaveta) == 2
+<<<<<<< HEAD
 >>>>>>> c5d29d9 (test: create first true test)
+=======
+
+def test_example2():
+    baralho = ["2", "3", "4", "5", "6", "7"]
+    gaveta = ["7", "3","4", "4", "4", "4","5","5","3","4", "7", "4", "2","3","3","5", "4", "4","2","7","7"]
+    
+    assert countingCards(baralho, gaveta) == 0
+>>>>>>> a82afc4 (test: create first false test)
