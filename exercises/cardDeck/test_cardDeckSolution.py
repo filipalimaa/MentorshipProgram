c7b60cb6d513485eb0a8eeb52dfab3dc93db8f9a
filipalimaa <filipalima1995@gmail.com>
2025-04-29 11:24:49 +0100

    test: create first true test

--- exercises/cardDeck/test_cardDeckSolution.py
+++ exercises/cardDeck/test_cardDeckSolution.py
@@ -1,4 +1,5 @@
 from cardDeckSolution import countingCards
+<<<<<<< HEAD
 from cardDeckSolution import counting_Cards
 
 
@@ -71,3 +72,11 @@ def test_example3():
 
     assert countingCards(baralho, gaveta) == 0
     assert counting_Cards(baralho, gaveta) == 0
+=======
+
+def test_example1():
+    baralho = ["2", "3", "4", "5", "6", "7"]
+    gaveta = ["6","7", "3","4", "4", "4", "4","5","5","3", "4", "7", "4","6", "6", "2","3","3","5", "4", "4","2","7","7"]
+    
+    assert countingCards(baralho, gaveta) == 2
+>>>>>>> c5d29d9 (test: create first true test)
