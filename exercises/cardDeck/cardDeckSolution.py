f98f25981edd813d79a65acc8ca8703d8379a67c
filipalimaa <filipalima1995@gmail.com>
2025-04-29 17:09:10 +0100

    feat: count the cards in the drawer

--- exercises/cardDeck/cardDeckSolution.py
+++ exercises/cardDeck/cardDeckSolution.py
@@ -1,2 +1,8 @@
 def countingCards(baralho, gaveta):
+    # Count the cards in the drawer
     countCards = {}
+    for carta in gaveta:
+        if carta in countCards:
+            countCards[carta] += 1
+        else:
+            countCards[carta] = 1
