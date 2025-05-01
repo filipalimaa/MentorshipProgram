452e8c6251648b14a7e7c6e7c3764f0cafd43d61
filipalimaa <filipalima1995@gmail.com>
2025-05-01 12:02:52 +0100

    build: change github actions

--- exercises/cardDeck/cardDeckSolution.py
+++ exercises/cardDeck/cardDeckSolution.py
@@ -5,15 +5,13 @@ def countingCards(deck, drawer):
         if card in countCardsDrawer:
             countCardsDrawer[card] += 1
         else:
-            countCardsDrawer[card] = 1
-    
+            countCardsDrawer[carta] = 1
+
     # count as few decks as possible
     countCardsDeck = []
     for carta in baralho:
         if carta not in countCardsDrawer:
             return 0
-        count = countCardsDrawer[card]
-        if min_decks is None or count < min_decks:
-            min_decks = count
-    
+        countCardsDeck.append(countCardsDrawer.get(carta, 0))
+
     return min(countCardsDeck)
--- exercises/wordSearch/test_cobraSolution.py
+++ exercises/wordSearch/test_cobraSolution.py
@@ -5,38 +5,34 @@ from cobraSolution import exist
 
 ## Outros possíveis testes: boards maiores ou vazios (?)
 
+
 def test_exist_true():
-    
-    board = [["A","B","C","E"],
-             ["S","F","C","K"],
-             ["A","D","E","E"]]
-    
+
+    board = [["A", "B", "C", "E"], ["S", "F", "C", "K"], ["A", "D", "E", "E"]]
+
     word = "BCCE"
     assert exist(board, word) == True
 
+
 def test_exist_false():
-    
-    board = [["A","B","C","E"],
-             ["S","F","C","K"],
-             ["A","D","E","E"]]
-    
+
+    board = [["A", "B", "C", "E"], ["S", "F", "C", "K"], ["A", "D", "E", "E"]]
+
     word = "ABCD"
     assert exist(board, word) == False
 
+
 def test_exist_two_letter():
-    
-    board = [["A","B","C","E"],
-             ["S","F","C","K"],
-             ["A","D","E","E"]]
-    
+
+    board = [["A", "B", "C", "E"], ["S", "F", "C", "K"], ["A", "D", "E", "E"]]
+
     word = "FD"
     assert exist(board, word) == True
 
+
 def test_exist_not_present():
-    
-    board = [["A","B","C","E"],
-             ["S","F","C","K"],
-             ["A","D","E","E"]]
-    
+
+    board = [["A", "B", "C", "E"], ["S", "F", "C", "K"], ["A", "D", "E", "E"]]
+
     word = "ILMN"
     assert exist(board, word) == False
