1b2f8bfe4987bb4b444cf74ec9bbd120f4636d43
filipalimaa <filipalima1995@gmail.com>
2025-05-03 17:12:52 +0100

    fix: fix error with the code from cobraSolution.py file

--- exercises/wordSearch/cobraSolution.py
+++ exercises/wordSearch/cobraSolution.py
@@ -42,7 +42,7 @@ def exist(board, word):
         ):
             return False
 
-        visited.add(row, col)
+        visited.add((row, col))
 
         if (
             search(row + 1, col, index + 1, visited)
