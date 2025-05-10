43372cf9a79d8a8b2d33954b7db6485bf599fa04
filipalimaa <filipalima1995@gmail.com>
2025-05-10 20:40:40 +0100

    test: add alternative function solution for the cardDeck for pytest

--- exercises/cardDeck/test_cardDeckSolution.py
+++ exercises/cardDeck/test_cardDeckSolution.py
@@ -1,5 +1,6 @@
 from cardDeckSolution import countingCards
 <<<<<<< HEAD
+<<<<<<< HEAD
 from cardDeckSolution import counting_Cards
 
 
@@ -73,6 +74,9 @@ def test_example3():
     assert countingCards(baralho, gaveta) == 0
     assert counting_Cards(baralho, gaveta) == 0
 =======
+=======
+from cardDeckSolution import counting_Cards
+>>>>>>> 7851fdf (test: add alternative function solution for the cardDeck for pytest)
 
 def test_example1():
     baralho = ["2", "3", "4", "5", "6", "7"]
@@ -80,8 +84,12 @@ def test_example1():
     
     assert countingCards(baralho, gaveta) == 2
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> c5d29d9 (test: create first true test)
 =======
+=======
+    assert counting_Cards(baralho, gaveta) == 2
+>>>>>>> 7851fdf (test: add alternative function solution for the cardDeck for pytest)
 
 def test_example2():
     baralho = ["2", "3", "4", "5", "6", "7"]
@@ -89,12 +97,20 @@ def test_example2():
     
     assert countingCards(baralho, gaveta) == 0
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> a82afc4 (test: create first false test)
 =======
+=======
+    assert counting_Cards(baralho, gaveta) == 0
+>>>>>>> 7851fdf (test: add alternative function solution for the cardDeck for pytest)
 
 def test_example3():
     baralho = ["2", "3", "4", "5", "6", "7"]
     gaveta = []
     
     assert countingCards(baralho, gaveta) == 0
+<<<<<<< HEAD
 >>>>>>> 54018f5 (test: create false test with empty array)
+=======
+    assert counting_Cards(baralho, gaveta) == 0
+>>>>>>> 7851fdf (test: add alternative function solution for the cardDeck for pytest)
