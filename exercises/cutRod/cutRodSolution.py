6ca7b6697d5deed25eb648d6af08b263acd4e831
filipalimaa <filipalima1995@gmail.com>
2025-05-11 15:12:09 +0100

    feat: create rod function to return the max value

--- exercises/cutRod/cutRodSolution.py
+++ exercises/cutRod/cutRodSolution.py
@@ -1,3 +1,15 @@
 def cutRod(price):
     rod_length = len(price)
-    return rod_length
+    return rod(price, rod_length)
+
+def rod(price, remain):
+    if remain == 0:
+        return 0
+    
+    max_value = None
+    
+    for i in range(1, remain + 1):
+        value = price[i -1] + rod(price, remain - i)
+        max_value = max(max_value, value)
+    
+    return max_value
