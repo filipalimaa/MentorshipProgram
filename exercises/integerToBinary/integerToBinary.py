5744d122e1d9153ce2435cbe3497e3f1f9669e8e
filipalimaa <filipalima1995@gmail.com>
2025-05-13 16:00:23 +0100

    feat: change the function by dividing by 2 and the remainder

--- exercises/integerToBinary/integerToBinary.py
+++ exercises/integerToBinary/integerToBinary.py
@@ -1,5 +1,8 @@
 # Conversion integer number to binary
 def integer_to_binary(number):
-    #binary = bin(number)
-    binary = format(number, 'b')
-    return binary
+    if number == 0:
+        return '0'
+    if number == 1:
+        return '1'
+    
+    return integer_to_binary(number // 2) + str(number % 2)
