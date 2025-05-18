fb9350f623ebcd00e44ee9d0b32126730ff5e778
filipalimaa <filipalima1995@gmail.com>
2025-05-18 22:04:11 +0100

    feat: change the function so that the result returned is numeric and not a string

--- exercises/integerToBinary/integerToBinary.py
+++ exercises/integerToBinary/integerToBinary.py
@@ -1,8 +1,8 @@
 # Conversion integer number to binary
 def integer_to_binary(number):
     if number == 0:
-        return '0'
+        return 0
     if number == 1:
-        return '1'
+        return 1
     
-    return integer_to_binary(number // 2) + str(number % 2)
+    return integer_to_binary(number // 2) + (number % 2)
