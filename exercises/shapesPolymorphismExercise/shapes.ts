c01b76eb5b1af5a7b0b6705551dcbe52956c659a
filipalimaa <filipalima1995@gmail.com>
2025-06-05 23:45:28 +0100

    feat: create subclass for triangle

--- exercises/shapesPolymorphismExercise/shapes.ts
+++ exercises/shapesPolymorphismExercise/shapes.ts
@@ -2,3 +2,26 @@ abstract class shape {
     abstract get_area(): number;
     abstract get_perimeter(): number;
 }
+
+class triangle extends shape {
+    side_A: number;
+    side_B: number;
+    side_C: number;
+    height: number;
+
+    constructor(a: number, b: number, c: number, height: number) {
+        super();
+        this.side_A = a;
+        this.side_B = b;
+        this.side_C = c;
+        this.height = height
+    }
+
+    get_area(): number {
+        return (this.side_A * this.height) / 2;
+    }
+
+    get_perimeter(): number {
+        return (this.side_A + this.side_B + this.side_C);
+    }
+}
