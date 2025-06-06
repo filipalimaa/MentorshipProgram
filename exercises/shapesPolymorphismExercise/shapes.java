18f34aa6be6b934adb79e8db6ca3806b80d60b7c
filipalimaa <filipalima1995@gmail.com>
2025-06-06 11:55:35 +0100

    feat: create class for triangle format

--- exercises/shapesPolymorphismExercise/shapes.java
+++ exercises/shapesPolymorphismExercise/shapes.java
@@ -2,3 +2,22 @@ abstract class Shape {
     abstract double getArea();
     abstract double getPerimeter();
 }
+
+class Triangle extends Shape {
+    double sideA, sideB, sideC, height;
+
+    Triangle(double a, double b, double c, double h) {
+        this.sideA = a;
+        this.sideB = b;
+        this.sideC = c;
+        this.height = h;
+    }
+
+    double getArea() {
+        return (sideA * height) / 2;
+    }
+
+    double getPerimeter() {
+        return sideA + sideB + sideC;
+    }
+}
