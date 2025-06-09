1ec34f091900bdc1c8f1c17cab9855668bd3174d
filipalimaa <filipalima1995@gmail.com>
2025-06-09 23:23:10 +0100

    feat: create a function to insert node in a list

--- exercises/linkedLists/linkedLists.py
+++ exercises/linkedLists/linkedLists.py
@@ -3,3 +3,18 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+#Create function to feed the linked list
+def insert(head, value):
+    new_node = Node(value)
+    
+    if not head:
+        return new_node
+    
+    current = head
+    
+    while current.next:
+        current = current.next
+        
+    current.next = new_node
+    return head
