f159d3e51f477742789ae253b1744241516b9a99
filipalimaa <filipalima1995@gmail.com>
2025-06-17 11:52:51 +0100

    feat: implement method to print the array

--- exercises/bubbleSortExample/bubble_sort_Ex.java
+++ exercises/bubbleSortExample/bubble_sort_Ex.java
@@ -14,4 +14,11 @@ public class BubbleSort {
             }
         }
     }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+        
 }
