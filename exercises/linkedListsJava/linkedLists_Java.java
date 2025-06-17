6a3f5dda121615da508106c17d40d40d8c117d9e
filipalimaa <filipalima1995@gmail.com>
2025-06-17 10:29:41 +0100

    feat: create linkedList class to insert node in a list

--- exercises/linkedListsJava/linkedLists_Java.java
+++ exercises/linkedListsJava/linkedLists_Java.java
@@ -7,3 +7,30 @@ class Node {
         this.next = null;
     }
 }
+
+class LinkedList {
+    Node head;
+
+    void insertAtEnd(int value) {
+        Node newNode = new Node(value);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+
+        current.next = newNode;
+    }
+
+    void printRecursive(Node node) {
+        if (node == null) return;
+
+        System.out.print(node.value);  // Imprime sem usar +
+        printRecursive(node.next);
+    }
+}
