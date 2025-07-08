3c3a6961404979d598288f9c66f3f4974d3a754f
filipaalima <139570730+filipalimaa@users.noreply.github.com>
2025-07-08 22:51:57 +0100

    feat: implement first part of the function to catch rotten oranges

--- exercises/rottenOranges/rottenOranges_graphsBFS.java
+++ exercises/rottenOranges/rottenOranges_graphsBFS.java
@@ -11,4 +11,21 @@ public class rottenOranges {
         }
     }
 
+    public static void rotten(int[][] matrix) {
+        int linhas = matrix.length;
+        int colunas = matrix[0].length;
+
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        Queue<position> q = new LinkedList<>();
+
+        for ( int i = 0; i < linhas; i++ ) {
+            for ( int j = 0; j < colunas; j++ ) {
+                if ( matrix[i][j] == 2 ) {
+                    q.add(new position(i,j));
+                }
+            }
+        }
+    }
 }
