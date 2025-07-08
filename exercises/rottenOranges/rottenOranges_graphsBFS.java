6fa3df94127ff2bc2d3691f5327bb59e4c4c1f8b
filipaalima <139570730+filipalimaa@users.noreply.github.com>
2025-07-08 22:46:14 +0100

    feat: implement aux class to represent the lines and columns

---
+++ exercises/rottenOranges/rottenOranges_graphsBFS.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class rottenOranges {
+
+    static class position {
+        int line, column;
+
+        position(int l,  int c) {
+            this.line = l;
+            this.column = c;
+        }
+    }
+
+}
