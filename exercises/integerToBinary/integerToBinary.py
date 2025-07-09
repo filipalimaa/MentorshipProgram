485d2aac4c09c42e6030b5df92b0541c88577d69
filipaalima <139570730+filipalimaa@users.noreply.github.com>
2025-07-09 22:26:51 +0100

    feat: add recursive integer to binary converter with pytest tests (#10)
    * docs: create integerToBinary file to store exercise solution and test_integerToBinary.py to store unit tests
    
    * feat: create basic function to initiate
    
    * test: add first unit test to test code
    
    * feat: use bin command to convert number to binary
    
    * feat: change 'bin' command for 'format' command for the conversion
    
    * fix: correct the unit test so that the result is returned in string format
    
    * feat: change the function by dividing by 2 and the remainder
    
    * feat: change the function so that the result returned is numeric and not a string
    
    * test: change the test function to a numeric value instead of a string
    
    * feat: multiply by 10 to 'shift' the binary digits one place to the left

---
+++ exercises/integerToBinary/integerToBinary.py
@@ -0,0 +1,8 @@
+# Conversion integer number to binary
+def integer_to_binary(number):
+    if number == 0:
+        return 0
+    if number == 1:
+        return 1
+    
+    return integer_to_binary(number // 2) * 10 + (number % 2)
